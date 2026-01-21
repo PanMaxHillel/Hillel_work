b167eb789b0e77f33ce05037be0a7243517b884c
PanMaxHillel <maksimventura@gmail.com>
2026-01-21 23:48:43 +0200

    Commit HomeWork

---
+++ 02/home work 01.py
@@ -0,0 +1,47 @@
+#Задание №1 "Квадрат числа"
+#print("Квадрат числа")
+
+#user_input = int (input("Введите число:"))
+
+#print(user_input ** 2)
+
+#Задание №2 "Среднее трех чисел"
+#print("Среднее трех чисел")
+
+#user_input_1 = int( input("Введите первую цифру:"))
+#user_input_2 = int( input("Введите вторую цифру:"))
+#user_input_3 = int( input("Введите третью цифру:"))
+
+#print( (user_input_1 + user_input_2 + user_input_3) // 3 )
+
+#Задание №3 "Конвертирование минут в часы"
+#print("Конвертирование минут в часы")
+
+#USER_INPUT_1 = int (input("Введите минуты:"))
+
+#print( int(USER_INPUT_1 / 60), end=" часа ")
+#print( int((USER_INPUT_1 % 60)), end= " минут ")
+
+#Задание №4 "Калькуляция скидки"
+#print("Калькуляция скидки")
+
+#user_input_1 = int( input("Введите цену:"))
+#user_input_2 = int( input("Введите скидку:"))
+
+#print("Сумма со скидкой:", float(user_input_1 - (user_input_1 * user_input_2) / 100))
+
+#Задание №5 "Последняя цифра числа"
+#print("Последняя цифра числа")
+
+#user_cifra_1 = int( input("Введите число:"))
+
+#print( user_cifra_1 % 10)
+
+#Задание №6 "Периметр прямоугольника"
+#print("Периметр прямоугольника")
+
+#user_dolgota_1 = int( input("Введите длинну:"))
+#user_shirota_2 = int( input("Введите ширину:"))
+
+#print("Периметр прямоугольника равен:", int( 2* (user_dolgota_1 + user_shirota_2)))
+
