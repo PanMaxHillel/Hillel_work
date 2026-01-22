8ce1c6c0d0f74ba6e264b20311eb411d996756a4
PanMaxHillel <maksimventura@gmail.com>
2026-01-22 18:48:31 +0200

    update

--- 02/home work 01.py
+++ 02/home work 01.py
@@ -1,47 +1,64 @@
 #Задание №1 "Квадрат числа"
-#print("Квадрат числа")
+print("Квадрат числа")
 
-#user_input = int (input("Введите число:"))
+user_input = int (input("Введите число:"))
 
-#print(user_input ** 2)
+print(user_input ** 2)
 
 #Задание №2 "Среднее трех чисел"
-#print("Среднее трех чисел")
+print("Среднее трех чисел")
 
-#user_input_1 = int( input("Введите первую цифру:"))
-#user_input_2 = int( input("Введите вторую цифру:"))
-#user_input_3 = int( input("Введите третью цифру:"))
+user_input_1 = int( input("Введите первую цифру:"))
+user_input_2 = int( input("Введите вторую цифру:"))
+user_input_3 = int( input("Введите третью цифру:"))
 
-#print( (user_input_1 + user_input_2 + user_input_3) // 3 )
+print( (user_input_1 + user_input_2 + user_input_3) // 3 )
 
 #Задание №3 "Перевод минут в часы"
-#print("Перевод минут в часы")
+print("Перевод минут в часы")
 
-#USER_INPUT_1 = int (input("Введите минуты:"))
+USER_INPUT_1 = int (input("Введите минуты:"))
 
-#print( int(USER_INPUT_1 / 60), end=" часа ")
-#print( int((USER_INPUT_1 % 60)), end= " минут ")
+print( int(USER_INPUT_1 / 60), end=" часа ")
+print( int((USER_INPUT_1 % 60)), end= " минут ")
+print("")
 
 #Задание №4 "Калькуляция скидки"
-#print("Калькуляция скидки")
+print("Калькуляция скидки")
 
-#user_input_1 = int( input("Введите цену:"))
-#user_input_2 = int( input("Введите скидку:"))
+user_input_1 = int( input("Введите цену:"))
+user_input_2 = int( input("Введите размер скидки (без %):"))
 
-#print("Сумма со скидкой:", float(user_input_1 - (user_input_1 * user_input_2) / 100))
+print("Сумма со скидкой:", float(user_input_1 - (user_input_1 * user_input_2) / 100))
 
 #Задание №5 "Последняя цифра числа"
-#print("Последняя цифра числа")
+print("Последняя цифра числа")
 
-#user_cifra_1 = int( input("Введите число:"))
+user_cifra_1 = int( input("Введите число:"))
 
-#print( user_cifra_1 % 10)
+print("Последняя цифра", user_cifra_1 % 10)
 
 #Задание №6 "Периметр прямоугольника"
-#print("Периметр прямоугольника")
+print("Периметр прямоугольника")
 
-#user_dolgota_1 = int( input("Введите длинну:"))
-#user_shirota_2 = int( input("Введите ширину:"))
+user_dlina_1 = int( input("Введите длину:"))
+user_shirina_2 = int( input("Введите ширину:"))
 
-#print("Периметр прямоугольника равен:", int( 2* (user_dolgota_1 + user_shirota_2)))
+print("Периметр прямоугольника равен:", int( 2* (user_dlina_1 + user_shirina_2)))
+
+#Задание №7 "Вывод чисел в столбец"
+
+print("Вывод чисел в столбец")
+
+user_cifra = int( input("Введите число, которое нужно разбить в стобик: "))
+
+a = user_cifra // 1000
+b = (user_cifra // 100) % 10
+c = (user_cifra // 10) % 10
+d = user_cifra % 10
+
+print("Первая цифра: ", a)
+print("Вторая цифра:",b)
+print("Третья цифра:",c)
+print("Четвертая цифра:",d)
 
