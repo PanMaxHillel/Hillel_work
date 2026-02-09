36629cb7345f58cf4ae73c21a549f7437d29c388
PanMaxHillel <maksimventura@gmail.com>
2026-02-09 18:17:43 +0200

    hw5.1

---
+++ HomeWork/HW05(3).py
@@ -0,0 +1,16 @@
+import string
+
+us_post = input ("VVed: ")
+
+clear_post = ""
+for ch in us_post:
+    if ch not in string.punctuation:
+        clear_post += ch
+
+words_01 = clear_post.title()
+hashtag1 = "#" + words_01.replace(" ", "")
+
+if len(hashtag1) > 140:
+    hashtag1 = hashtag1[:140]
+
+print(hashtag1)
