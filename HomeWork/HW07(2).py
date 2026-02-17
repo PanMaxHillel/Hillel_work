1c9a5ad03689eb9e128ba13a355d66ee45700ac8
PanMaxHillel <maksimventura@gmail.com>
2026-02-17 14:52:02 +0200

    hw7.2

--- HomeWork/HW07(2).py
+++ HomeWork/HW07(2).py
@@ -1,14 +1,13 @@
 def correct_sentence(sentence: str) -> str:
-    sentence = sentence.capitalize()
+    sentence = sentence[0].upper() + sentence[1:]
     if not sentence.endswith('.'):
-        sentence = sentence + '.'
-
+        sentence += '.'
     return sentence
 
-assert correct_sentence("greetings, friends")
-assert correct_sentence("hello")
-assert correct_sentence("Greetings. Friends")
-assert correct_sentence("Greetings, friends.")
-assert correct_sentence("greetings, friends.")
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
 
-print("OK")
+print('ОК')
